955c629dd5a40c5f6f1eeda9b5fc8bbf1094afa6
llucasreis <pro.lucasreis@gmail.com>
2021-10-15 20:44:31 -0300

    feat: use env var from heroku or local

--- src/client/azure.py
+++ src/client/azure.py
@@ -1,10 +1,11 @@
 import requests
 import os
+from utils.env import get_env_var
 
 class Azure:
   def __init__(self) -> None:
-      self.url = os.getenv('AZURE_URL')
-      api_key = os.getenv('AZURE_API_KEY')
+      self.url = get_env_var('AZURE_URL')
+      api_key = get_env_var('AZURE_API_KEY')
       self.headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
   def predict(self, body: str) -> str:
       response = requests.post(url=self.url, headers=self.headers, data=body)
