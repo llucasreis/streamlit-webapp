c2a27301601b803b4e3206f8ec7a97b905e73660
llucasreis <pro.lucasreis@gmail.com>
2021-10-15 20:38:58 -0300

    fix: correct azure env

--- src/client/azure.py
+++ src/client/azure.py
@@ -1,11 +1,10 @@
 import requests
-from utils.env import load_env
+import os
 
 class Azure:
   def __init__(self) -> None:
-      env = load_env()
-      self.url = env['AZURE_URL']
-      api_key = env['AZURE_API_KEY']
+      self.url = os.getenv('AZURE_URL')
+      api_key = os.getenv('AZURE_API_KEY')
       self.headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
   def predict(self, body: str) -> str:
       response = requests.post(url=self.url, headers=self.headers, data=body)
