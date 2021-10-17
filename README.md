3af175e9b6c8303ebbf2e227a37e7344f0d52934
llucasreis <pro.lucasreis@gmail.com>
2021-10-16 21:34:22 -0300

    fix wrong name

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Streamlit WebApp
+# Lung Cancer Prediction
+
+Este é um Data App para Diagnóstico preditivo de Câncer de Pulmão
+
+Desenvolvido pelos alunos: Andrea Monicque, Francisco Marcelo, Lucas Pereira e Marcos Wenneton para a discplina de Infraestrutura em Nuvem para Projetos com Ciência dos Dados do curso de Pós-Graduação em Ciência de Dados da Universidade do Estado do Amazonas - UEA.
 
 ```
 pip install -r requirements.txt
--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from client.azure import Azure
 
 st.title("Lung Cancer Prediction")
 st.markdown("Este é um Data App para Diagnóstico preditivo de Câncer de Pulmão")
-st.markdown("Desenvolvido pelos alunos: Andrea Mendonça, Francisco Marcelo, Lucas Pereira e Marcos Wenneton " \
+st.markdown("Desenvolvido pelos alunos: Andrea Monicque, Francisco Marcelo, Lucas Pereira e Marcos Wenneton " \
             "para a discplina de Infraestrutura em Nuvem para Projetos com Ciência dos Dados do curso de Pós-Graduação "\
             "em Ciência de Dados da Universidade do Estado do Amazonas - UEA.")
 
